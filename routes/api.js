1788272208f435b33bbd7df18896b4155318dd24
Isale <isale.bryan@gmail.com>
2020-06-22 11:54:14 +0300

    Post request url

--- routes/api.js
+++ routes/api.js
@@ -15,44 +15,47 @@ var ObjectId = require('mongodb').ObjectID;
 require('dotenv').config()
 
 const CONNECTION_STRING = process.env.DB; 
-MongoClient.connect(CONNECTION_STRING, function(err, client) {
-  let db = client.db('issue-tracker')
-  if (err) {
-    return console.log("An error occured ", err)
-  }
-  console.log("Connected to database");
-  
-});
+//MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      let title = req.body.issue_title;
-      let text = req.body.issue_text;
-      let creator= req.body.created_by;
-      let assignedTo = req.body.assigned_to;
-      let status = req.body.status_text
-
+      var query = req.query
       var project = req.params.project;
 
-      res.send('Hi')
+      MongoClient.connect(CONNECTION_STRING, (err, client)=>{
+
+      })
       
       
     })
     
     .post(function (req, res){
-      let title = req.body.issue_title;
-      let text = req.body.issue_text;
-      let creator= req.body.created_by;
-      let assignedTo = req.body.assigned_to;
-      let status = req.body.status_text
       var project = req.params.project;
+      let issue = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to,
+      status_text: req.body.status_text,
+      created_on: new Date(),
+      updated_on: new Date(),
+      open: true
+      }
 
-      console.log(creator);
-      
-      
+      MongoClient.connect(CONNECTION_STRING, (err, client)=>{
+        let db = client.db('issue-tracker');
+        db.collection(project).insertOne(issue, function(err, doc) {
+          if (err) {
+            console.error('Errror encountered', err);
+            
+          } else {
+            
+          }
+        })
+      })
       
     })
     
