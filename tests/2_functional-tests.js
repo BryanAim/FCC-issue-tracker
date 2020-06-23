5dba14545406675afa4f812ea1b2c6fc3729e009
Isale <isale.bryan@gmail.com>
2020-06-23 10:13:20 +0300

    Multiple filters functional test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -171,6 +171,24 @@ suite('Functional Tests', function() {
       });
       
       test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
+        chai.request(server)
+        .get('/api/issues/test')
+        .query({issue_text: 'test'})
+        .end(function(err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body[0].issue_text, 'text');
+          assert.property(res.body[0], 'issue_title');
+          assert.property(res.body[0], 'issue_text');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'updated_on');
+          assert.property(res.body[0], 'created_by');
+          assert.property(res.body[0], 'assigned_to');
+          assert.property(res.body[0], 'open');
+          assert.property(res.body[0], 'status_text');
+          assert.property(res.body[0], '_id');
+
+          done()
+        })
         
       });
       
